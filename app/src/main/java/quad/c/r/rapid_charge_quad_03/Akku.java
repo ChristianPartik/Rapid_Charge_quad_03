6e72d84a20fd3333369a70141d3692384a329088
ChristianPartik <christian.partik@gmail.com>
2016-03-05 22:22:06 +0100

    Rapid Charge Quad APP + Maps

--- app/src/main/java/quad/c/r/rapid_charge_quad_03/Akku.java
+++ app/src/main/java/quad/c/r/rapid_charge_quad_03/Akku.java
@@ -1,20 +1,21 @@
 package quad.c.r.rapid_charge_quad_03;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Beleuchtung extends Fragment {
+/**
+ * Created by Christian on 05.03.2016.
+ */
+public class Akku extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.activity_beleuchtung, container,false);
-
+        View rootview = inflater.inflate(R.layout.activity_akku, container, false);
         return rootview;
     }
 }
--- app/src/main/java/quad/c/r/rapid_charge_quad_03/MainActivity.java
+++ app/src/main/java/quad/c/r/rapid_charge_quad_03/MainActivity.java
@@ -1,11 +1,12 @@
 package quad.c.r.rapid_charge_quad_03;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -75,16 +78,23 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.drawer_layout, new Akku()).commit();
 
         if (id == R.id.nav_akku) {
 
+            fragmentManager.beginTransaction().replace(R.id.drawer_layout, new Akku()).commit();
 
 
         } else if (id == R.id.nav_beleuchtung) {
-            position = 1;
-        }
 
+            fragmentManager.beginTransaction().replace(R.id.drawer_layout, new Beleuchtung()).commit();
+
+
+        } else if (id == R.id.nav_Karte) {
+
+        fragmentManager.beginTransaction().replace(R.id.drawer_layout, new MapsActivity()).commit();
+        }
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
